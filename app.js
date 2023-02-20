ba9107db44dae9ee40f41a35f4e24c216eb662a5
nikilneosoft <nikil.kurian@neosoftmail.com>
2023-02-20 16:56:51 +0530

    adding morgan and mongoose

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const AuthRoute = require('./routes/auth.route');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(morgan('dev'));
 app.listen(port,()=>{
     console.log(`Server running on ${port}`);
 });
